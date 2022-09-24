1930989a098ff2f17badfdeab52acb64c56aa6ac
“Trevor” <Trevrichmond@gmail.com>
2022-09-24 15:10:18 -0500

    Customer class

--- main.py
+++ main.py
@@ -1,4 +1,40 @@
 # Computer Vision to improve shit--
+from operator import itemgetter
+import string
+import struct
+
+
 def main():
     print("I Totes see you!") # Hehe
 main()
+class Customer:
+    def __init__(self, uuid):
+        self.uuid = uuid
+
+        customer_first_name = ""
+
+        customer_last_name = ""
+
+        gender = ""
+
+        age = ""
+
+        customer_email = ""
+
+        customer_phone = ""
+
+class StoreVisit:
+    def __init__(self, visits):
+        self.visits = visits
+
+        date_visited = ""
+
+        time_in_store = 0.0
+        
+        money_spent = 0.0
+
+        items_bought = []
+        
+        time_spent_aisle_num = []          
+         
+        
