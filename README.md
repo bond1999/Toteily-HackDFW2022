a49eed50d251b7e7442921d8c1c86869d8c99bc5
bond1999 (Surya) <46317327+bond1999@users.noreply.github.com>
2022-09-24 13:38:26 -0500

    Added requirements.txt README.md changed main.py added function templates

--- README.md
+++ README.md
@@ -1,7 +1,20 @@
-# HackDFW2022
-Blake
-Hunter
-Nate
-Trevor
-Surya
-Mergin smergin
+# Tot(e)ally bags
+
+## How does it work?
+
+## Why do you need our product?
+
+## Requirements:
+
+## How to run:
+
+## Members:
+Blake Lawyer 
+
+Hunter Girvan
+
+Nate Montgomery
+
+Surya Aggarwal
+
+Trevor Richmond
--- main.py
+++ main.py
@@ -1,4 +1,15 @@
-# Computer Vision to improve shit--
+# Project Idea: Computer Vision and AI to create the pathway for a Personalized Customer Journey
+# Team Totebags
+# Store Function
+def store():
+    print("Storing Shopping Information...")
+    print("Storing Successful!")
+
+# Retrieve Function
+def retrieve():
+    print("Retrieving Shopping Information...")
+    print("Retrieving Successful!")
+
 def main():
     print("I Totes see you!") # Hehe
 main()
