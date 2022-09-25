fdf1f8034ec80e38a113dd40c9b882a0562b6945
blakethelawyer <blakethelawyer@gmail.com>
2022-09-24 20:36:06 -0500

    finalized processor w/ video output

--- customer.py
+++ customer.py
@@ -1,10 +1,13 @@
 class Customer:
-    first_name = None
-    last_name = None
-    gender = None
-    age = None
-    customer_email = None
-    customer_phone = None
-    visits = []
     def __init__(self, uuid):
         self.uuid = uuid
+
+        if self.uuid == "3c31d93c-a668-4f24-b44e-91663b9a03e6":
+            self.first_name = "Trevor"
+            self.last_name = "Richmond"
+        elif self.uuid == "fe2342e4-c6dd-4828-9d7f-a277be7f57fe":
+            self.first_name = "Hunter"
+            self.last_name = "Girvan"
+
+        self.time_entered = None
+        self.time_exited = None
