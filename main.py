9116796b1913127d7410d6dfea8a1bad94d16f26
bond1999 (Surya) <46317327+bond1999@users.noreply.github.com>
2022-09-24 23:52:32 -0500

    readCustomerData() updated

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 # Project Idea: Computer Vision and AI to create the pathway for a Personalized Customer Journey
 # Computer Vision to improve shit--
+import random
 import uuid
-
 import customer
 import processor
 import csv
@@ -40,6 +40,13 @@ def readCustomerData():
         data = line.split(',')
         newCustomer = customer.Customer(data[0], data[1], data[2], data[3], data[4], data[5], data[6])
         allCustomers.append(newCustomer)
+    f2 = open("assets/MOCK_DATA STORE.csv")
+    f2.readline()
+    for line in f2:
+        data = line.split(',')
+        visit = customer.StoreVisit(data[0], data[1], data[2], data[3], data[4])
+        allCustomers[random.randint(0, 1001)].visits.append(visit)
+
     return allCustomers
 
 def writeCustomerData(customer):
@@ -53,5 +60,6 @@ def main():
     # processor.process()
     print("I Totes gotch your QR!") # Haha
     allCustomers = readCustomerData()
+    print(allCustomers[0].first_name, allCustomers[0].visits)
 
 main()
