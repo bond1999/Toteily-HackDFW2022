6b9cd5e856c33cd1b7c4cfe6d3add852ed173437
Nate-Montgomery <Nate.montogomery@gmail.com>
2022-09-25 02:24:27 -0700

    graphs.py stuff

--- graphs.py
+++ graphs.py
@@ -1,6 +1,16 @@
 import matplotlib.pyplot as plt
 import numpy as np
+import random
 
 def createGraphs(allCustomers):
-    plt.pie(pie_chart ,labels = my_labels)
+    timeNStore = []
+    numBought = []
+    for i in range(0, 15):
+        c = allCustomers[random.randint(0, 100)]
+        for v in c.visits:
+            timeNStore.append(v.time_in_store)
+            numBought.append(len(v.items_bought))
+            
+            
+    plt.plot(timeNStore, numBought)
     plt.show()
--- main.py
+++ main.py
@@ -8,6 +8,7 @@ import processor
 import csv
 
 import customer
+from graphs import createGraphs
 
 # UUID Generator
 def generateUUID():
@@ -33,6 +34,7 @@ def readCustomerData():
             allCustomers.append(newCustomer)
     with open("assets/MOCK_DATA_STORE_VISITS.csv") as f2:
         csvreader = csv.reader(f2, delimiter=',')
+        header = next(csvreader)
         for row in csvreader:
             visit = customer.StoreVisit(row)
             allCustomers[random.randint(0, 999)].visits.append(visit)
@@ -40,13 +42,18 @@ def readCustomerData():
 
 def main():
     allCustomers = readCustomerData()
-    print(len(allCustomers))
-    print("Customers Data Loading Complete!")
-    print("Starting Video Stream in 5 seconds.")
-    time.sleep(5)
-    processor.process(allCustomers)
-    print("I Totes gotch your QR!") # Haha
-    print(allCustomers[0].first_name)
+    print(len(allCustomers[0].visits))
+    
+    createGraphs(allCustomers)
+    
+    while 0:
+        print(len(allCustomers))
+        print("Customers Data Loading Complete!")
+        print("Starting Video Stream in 5 seconds.")
+        time.sleep(5)
+        processor.process(allCustomers)
+        print("I Totes gotch your QR!") # Haha
+        print(allCustomers[0].first_name)
     
 
 main()
