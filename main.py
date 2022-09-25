58a275fdf1509e7bf1ce9adef3ac631e98886089
bond1999 (Surya) <46317327+bond1999@users.noreply.github.com>
2022-09-24 19:56:20 -0500

    readCustomerData added and allCustomer accessible as objects

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ import processor
 # UUID Generator
 def generateUUID():
     flag = 0
-    f = open("assets/UUIDs.csv", "r+")
+    f = open("assets/MOCK_DATA_CUSTOMER.csv", "r+")
     newUUID = uuid.uuid4()
     for line in f:
         if line == newUUID:
@@ -28,20 +28,6 @@ def generateXUUIDs(X):
             generateUUID()
             count += 1
 
-
-# Store Function
-def save():
-    print("Storing Shopping Information...")
-
-    print("Storing Successful!")
-
-
-# Retrieve Function
-def retrieve():
-    print("Retrieving Shopping Information...")
-
-    print("Retrieving Successful!")
-
 class StoreVisit:
     date_visited = ""
     time_in_store = 0.0
@@ -57,15 +43,35 @@ class Customer:
     customer_email = None
     customer_phone = None
     visits = []
-    def __init__(self, uuid):
+    def __init__(self, uuid, fn, ln, g, a, ce, cp):
         self.uuid = uuid
+        self.first_name = fn
+        self.last_name = ln
+        self.gender = g
+        self.age = a
+        self.customer_email = ce
+        self.customer_phone = cp
 
+def readCustomerData():
+    f1 = open("../assets/MOCK_DATA_CUSTOMER.csv")
+    f1.readline()
+    allCustomers = []
+    for line in f1:
+        data = line.split(',')
+        newCustomer = Customer(data[0], data[1], data[2], data[3], data[4], data[5], data[6])
+        allCustomers.append(newCustomer)
+    return allCustomers
 
+def writeCustomerData(customer):
+    f3 = open("../assets/CUSTOMER_DATA.dat")
+
+    pass
 
 def main():
     generateXUUIDs(20)
     print("I Totes see you!") # Hehe
     processor.process()
-
+    print("I Totes gotch your QR!") # Haha
+    allCustomers = readCustomerData()
 
 main()
